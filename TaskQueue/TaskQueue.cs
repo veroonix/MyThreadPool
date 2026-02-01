2b3f991103a44c258d423ef4a8e09a2ea6bf2d03
veroonix <veranikastryzhak@gmail.com>
2026-02-01 21:24:36 +0300

    update working thread

--- TaskQueue/TaskQueue.cs
+++ TaskQueue/TaskQueue.cs
@@ -5,6 +5,7 @@ namespace TaskQueue
     public class TaskQueue
     {
         private readonly Thread[] _Threads;
+        private readonly ThreadPriority _Prioroty;
         private readonly AutoResetEvent _WorkingEvent = new(false);
         private readonly AutoResetEvent _ExecuteEvent = new(true);
         private readonly Queue<Action> _Works = new();
@@ -15,13 +16,23 @@ namespace TaskQueue
             if (MaxThreadsCount <= 0)
                 throw new ArgumentOutOfRangeException(nameof(MaxThreadsCount),MaxThreadsCount, "Число потоков в пуле потоков должно быть больше или равно 1" );
             _Threads = new Thread[MaxThreadsCount];
+            _Prioroty = ThreadPriority.Normal;
+            Initialize();
         }
 
         private void Initialize()
         {
+            
             for (int i = 0; i < _Threads.Length; i++)
             {
-                Thread thread = new Thread(WorkingThread);
+                var name = $"Thread[{i}]";
+                Thread thread = new Thread(WorkingThread)
+                {
+                    Name = name,
+                    IsBackground = true,
+                    Priority = _Prioroty
+
+                };
                 _Threads[i] = thread;
                 thread.Start();
             }
@@ -38,25 +49,48 @@ namespace TaskQueue
 
         private void WorkingThread()
         {
+            var threadName = Thread.CurrentThread.Name;
+            Trace.TraceInformation("Поток {0} запущен с id:{1}", threadName, Environment.CurrentManagedThreadId);
             while (true)
             {
                 _WorkingEvent.WaitOne();
+
                 _ExecuteEvent.WaitOne(); 
 
+                while (_Works.Count == 0)           //пока в очереди нет заданий
+                {
+                    _ExecuteEvent.Set(); 
+                    _WorkingEvent.WaitOne();
+                    _ExecuteEvent.WaitOne();
+                }
+
                 var work = _Works.Dequeue();
+                if (_Works.Count > 0)
+                {
+                    _WorkingEvent.Set();
+                }
                 _ExecuteEvent.Set(); 
 
+                Trace.TraceInformation(
+                    "Поток {0}[id:{1}] выполняет задание", 
+                    threadName, Environment.CurrentManagedThreadId);
                 try
                 {
-                     work();
+                    var timer = Stopwatch.StartNew();
+                    work();
+                    timer.Stop();
+
+                    Trace.TraceInformation(
+                        "Поток {0}[id:{1}] выполнил задание за {2}мс",
+                        threadName, Environment.CurrentManagedThreadId, timer.ElapsedMilliseconds);
                 }
                 catch (Exception e)
                 {
-                    Trace.TraceError("Ошибка выполнения задания в потоке", e);
+                    Trace.TraceError("Ошибка выполнения задания в потоке {0}:{1}", threadName, e);
                 }
             
             }
-            
+            Trace.TraceInformation("Поток {0} завершил свою работу", threadName);
         }
     }
 
